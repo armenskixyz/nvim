6f9378c5be0addac49daddd928c84391af18ab8f
armenskixyz <conk.dsc@onet.pl>
2025-11-23 20:04:04 +0100

    tabs

--- init.lua
+++ init.lua
@@ -173,6 +173,9 @@ vim.o.confirm = true
 
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
+--
+
+require 'keymap'
 
 -- Clear highlights on search when pressing <Esc> in normal mode
 --  See `:help hlsearch`
